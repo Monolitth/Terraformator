1a54cf507f4b0f67344e7a2f159812ffb46d4170
Monolitth <36070317+Monolitth@users.noreply.github.com>
2018-12-16 21:48:31 +0200

    Create terraformator.h

---
+++ terraformator.h
@@ -0,0 +1,78 @@
+#ifndef TERRAFORMATOR_H
+#define TERRAFORMATOR_H
+
+#include <QGLWidget>
+
+class Terraformator : public QGLWidget
+{
+public:
+    enum class eState
+    {
+        MENU,
+        PLAY,
+        LOAD,
+        SETTINGS,
+        SEEDG,
+        RANDG,
+        EXIT
+    };
+
+    enum class eMenu
+    {
+        START = -1,
+        PLAY,
+        LOAD,
+        SETTINGS,
+        EXIT,
+        END
+    };
+
+    enum class ePlay
+    {
+        START = -1,
+        GWORLDCHOOSEU,
+        GWORLDCHOOSER,
+        PLAY,
+        EXIT
+    };
+
+private:
+    eState mState{eState::MENU};
+    eMenu mCurrentMenu{eMenu::PLAY};
+    ePlay mPlay{ePlay::GWORLDCHOOSEU};
+
+    std::vector<std::pair<eMenu, std::string>> mvMenu;
+    std::vector<std::pair<ePlay, std::string>> mvPlay;
+    
+    std::vector<char> wName;
+    std::vector<int>  wSeed;
+    
+    bool nameinput{false};
+    bool seedinput{false};
+    
+    void _proccesing();
+    void _draw();
+
+    void _draw_menu();
+    void _draw_play();
+    void _draw_randg();
+    void _draw_seedg();
+
+    void _key_Released_menu(int);
+    void _key_Released_play(int);
+    void _key_Released_settings(int);
+    void _key_Released_load(int);
+    void _key_Released_randg(int);
+    void _key_Released_seedg(int);
+public:
+
+    Terraformator();
+
+    void InitializeGL();
+
+    void resizeGL(int,int)             override;
+    void paintGL()                     override;
+    void keyReleaseEvent(QKeyEvent*)   override;
+};
+
+#endif // TERRAFORMATOR_H
