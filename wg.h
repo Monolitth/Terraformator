f83c5b7de1c6368a4e9ab50891b02c0840c7f854
Monolitth <36070317+Monolitth@users.noreply.github.com>
2018-12-18 23:09:30 +0200

    Update wg.h

--- wg.h
+++ wg.h
@@ -1,18 +1,14 @@
-#include <time.h>
-#include <stdlib.h>
-#include <vector>
-using namespace std;
-
-
 #ifndef WG_H
 #define WG_H
+
+#include <vector>
 
 class wg
 {
 public:
 
-    void ds(vector<vector<int>> &map, int WS, int NHMax, int NHMin);
-    void ds(vector<vector<int>> &map, int WS, int NHMax, int NHMin, int seed);
+    void ds(std::vector<std::vector<int>>&map,int WS, int NHMax, int NHMin);
+    void ds(std::vector<std::vector<int>>&map,int WS, int NHMax, int NHMin, int seed);
 
     int getseednum(int seed);
 };
